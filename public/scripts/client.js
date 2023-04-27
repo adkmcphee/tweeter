9846f903b94a7e5dd7e9cf2639ee8d2f0f592db7
Adam McPhee <adkmcphee@gmail.com>
2023-04-27 22:21:03 +0000

    clean up code for readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,8 @@
  */
 
 $(document).ready(() => {
+
+  //function to render tweet from back-end, prepend to existing html
   const renderTweets = (tweets) => {
     for (const tweet of tweets) {
       const $tweetElement = createTweetElement(tweet);
@@ -12,12 +14,16 @@ $(document).ready(() => {
     }
   };
 
+
+  //function to prevent XSS attack
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+
+  //function to create dynamic tweet HTML
   const createTweetElement = (tweet) => {
     const $tweetElement = $(`
       <article>
@@ -42,11 +48,13 @@ $(document).ready(() => {
           </div>
         </footer>
       </article>
-  
-`);
+    
+      `);
 
     return $tweetElement;
+
   };
+
 
   //function to fetch tweets from /tweets
   const loadTweets = () => {
@@ -58,11 +66,14 @@ $(document).ready(() => {
     });
   };
 
+
   //make a GET request to load initial tweets
   loadTweets();
 
+
   // grab the form from the DOM
   const $form = $("#new-tweet-form");
+
 
   //add a submit handler to the form
   $form.on("submit", (event) => {
@@ -71,24 +82,34 @@ $(document).ready(() => {
     const data = $form.serialize();
     const tweetText = $form.find("#tweetForm").val();
 
+
+    //error message if tweet form is empty
     if (!tweetText) {
       $("#error-message").text("Please fill in the form below.").slideDown();
       return;
     }
 
+
+    //error message if tweet length exceeds maximum characters
     if (tweetText.length > maxCharacters) {
-      $("#error-message").text("Your tweet exceeds the maximum number of characters").slideDown();
+      $("#error-message")
+        .text("Your tweet exceeds the maximum number of characters")
+        .slideDown();
       return;
     }
+
 
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: data,
     }).then(() => {
+      loadTweets();
       $("#tweetForm").val("");
       $("#error-message").slideUp();
-      loadTweets();
+      $(".counter").val(maxCharacters);
+
+      
     });
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-  console.log('Document is ready');
 
   const tweetForm = document.getElementById('tweetForm');
   const maxCharacters = 140;
-  
 
   $('#tweetForm').on('input', function() {
     const totalCharacters = $(this).val().length;
